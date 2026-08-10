5e49a364bf92eb01536453d0483a37094a126e40
Cauê Rocha Pereira <caue.rocha@oddik.com.br>
2026-08-10 08:52:22 -0300

    Implement simple attack ability

---
+++ Source/ProjectPlataformer/AnimNotifies/SendGameplayEvent.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "SendGameplayEvent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTPLATAFORMER_API USendGameplayEvent : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GAS")
+	FGameplayTag EventTag;
+	
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+};
