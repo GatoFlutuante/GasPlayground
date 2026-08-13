ac33ceaaba1e95b466ad4ae53a029c65368f27d5
Cauê Rocha Pereira <caue.rocha@oddik.com.br>
2026-08-13 20:09:08 -0300

    Create base item data services

---
+++ Source/ProjectPlataformer/DataAssets/ItemData.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "Engine/DataAsset.h"
+#include "ItemData.generated.h"
+
+
+UCLASS(Blueprintable)
+class PROJECTPLATAFORMER_API UItemData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item")
+	USkeletalMesh* ItemMesh;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item")
+	FName AttachSocketName;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item")
+	TSubclassOf<UGameplayEffect> ItemGameplayEffect;
+};
---
+++ Source/ProjectPlataformer/Subsystems/EquipmentSubsystem.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DataAssets/ItemData.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "EquipmentSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTPLATAFORMER_API UEquipmentSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+	public:
+	UFUNCTION(BlueprintCallable, Category="Equipment")
+	UItemData* GetItemByID(FName ItemID);
+};
